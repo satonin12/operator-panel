e10d94dd3ba04c279d606b0998b6e01d2456a18b
Vladislav <vlad.satonin74@gmail.com>
2021-10-21 13:48:27 +0500

    add Yup & Form-level validation use Formik KIN-251

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 import LabelInput from '../../components/Inputs/LabelInput/LabelInput'
 import Button from '../../components/Button/Button'
 
 import './index.scss'
+
+const SignupSchema = Yup.object().shape({
+  email: Yup.string()
+    .email('Email должен иметь общепринятый вид адреса электронной почты')
+    .required('Email должен быть введен'),
+  password: Yup.string().required('Пароль должен быть введен'),
+})
 
 const LoginPage = () => {
   const formik = useFormik({
@@ -12,6 +20,7 @@ const LoginPage = () => {
       email: '',
       password: '',
     },
+    validationSchema: SignupSchema,
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2))
     },
@@ -26,22 +35,30 @@ const LoginPage = () => {
             <form onSubmit={formik.handleSubmit}>
               <div className="FormContent">
                 <LabelInput
-                  required
                   type="email"
                   label="email"
                   name="email"
                   value={formik.values.email}
                   onChange={formik.handleChange}
                 />
+                {formik.touched.email && formik.errors.email ? (
+                  <span className="formPrompt formPrompt--error">
+                    {formik.errors.email}
+                  </span>
+                ) : null}
 
                 <LabelInput
-                  required
                   name="password"
                   type="password"
                   label="password"
                   onChange={formik.handleChange}
                   value={formik.values.password}
                 />
+                {formik.touched.password && formik.errors.password ? (
+                  <span className="formPrompt formPrompt--error">
+                    {formik.errors.password}
+                  </span>
+                ) : null}
 
                 <Button type="submit" styleButton="primary">
                   Войти
