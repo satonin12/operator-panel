caf03bf70747adebb20883a1e448c2f01560f263
Vladislav <vlad.satonin74@gmail.com>
2021-10-28 13:44:53 +0500

    add component (form) forgot-password & refresh-password KIN-269

---
+++ src/components/forms/RefreshPasswordForm.js
@@ -0,0 +1,61 @@
+import React from 'react'
+
+import './index.scss'
+import LabelInput from '../Inputs/LabelInput/LabelInput'
+import Button from '../Button/Button'
+import { Link } from 'react-router-dom'
+import { RouteNames } from '../../router'
+
+const RefreshPasswordForm = ({ formik }) => {
+  return (
+    <>
+      <form onSubmit={formik.handleSubmit}>
+        <div className="FormContent">
+          <LabelInput
+            name="password"
+            type="password"
+            label="password"
+            placeholder=""
+            onChange={formik.handleChange}
+            value={formik.values.password}
+          />
+          {formik.touched.password && formik.errors.password ? (
+            <span className="formPrompt formPrompt--error">
+              {formik.errors.password}
+            </span>
+          ) : null}
+
+          <LabelInput
+            name="repeatPassword"
+            type="password"
+            label="confirm password"
+            placeholder=""
+            onChange={formik.handleChange}
+            value={formik.values.repeatPassword}
+          />
+          {formik.touched.repeatPassword && formik.errors.repeatPassword ? (
+            <span className="formPrompt formPrompt--error">
+              {formik.errors.repeatPassword}
+            </span>
+          ) : null}
+
+          <Button type="submit" styleButton="primary">
+            Отправить ссылку для восстановления
+          </Button>
+
+          <div className="formLinks">
+            <Link className="formLinks--item" to={RouteNames.LOGIN}>
+              Войти
+            </Link>
+
+            <Link className="formLinks--item" to={RouteNames.REGISTER}>
+              Регистрация
+            </Link>
+          </div>
+        </div>
+      </form>
+    </>
+  )
+}
+
+export default RefreshPasswordForm
