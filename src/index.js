51bb11c0992f0e456f5b8977d50e02d1f20fccf2
Vladislav <vlad.satonin74@gmail.com>
2021-11-05 13:58:21 +0500

    add redux-persist so that the token is not lost on reload KIN-291

--- src/index.js
+++ src/index.js
@@ -9,19 +9,34 @@ import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import { Provider } from 'react-redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { persistStore, persistReducer } from 'redux-persist'
+import LocalStorage from 'redux-persist/lib/storage'
 
 import { reducer } from './reducers'
 import rootSaga from './sagas/index'
 import { BrowserRouter } from 'react-router-dom'
+import { PersistGate } from 'redux-persist/integration/react'
+
+// create the persist Config
+const persistConfig = {
+  key: 'token',
+  storage: LocalStorage
+  // whitelist: ['SET_TOKEN'] // which reducer want to store
+}
+
+// wrap our main reducer in persist
+const pReducer = persistReducer(persistConfig, reducer)
 
 // create the saga middleware
 const sagaMiddleware = createSagaMiddleware()
 
 // create a redux store with our reducer above and middleware
 const store = createStore(
-  reducer,
+  pReducer,
   composeWithDevTools(applyMiddleware(sagaMiddleware))
 )
+
+const persistor = persistStore(store)
 
 // run the saga
 sagaMiddleware.run(rootSaga)
@@ -29,9 +44,11 @@ sagaMiddleware.run(rootSaga)
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
