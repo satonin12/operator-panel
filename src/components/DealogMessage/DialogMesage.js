78b3f061fa95645be6ce3d5a45940496c30dc7c7
Vladislav <vlad.satonin74@gmail.com>
2021-11-09 23:30:13 +0500

    styling & developming html markup on Dialog component, add DialogMessage react component KIN-304

---
+++ src/components/DealogMessage/DialogMesage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DialogMesage = () => {
+  return (
+    <div>Это сообщение, прикнь)</div>
+  )
+}
+
+export default DialogMesage
--- src/components/Dialog/Dialog.js
+++ src/components/Dialog/Dialog.js
@@ -1,10 +1,44 @@
 import React from 'react'
 
+import './index.scss'
+import LabelInput from '../Inputs/LabelInput/LabelInput'
+import Input from '../Inputs/Input/Input'
+import DialogMessage from '../DealogMessage/DialogMesage'
+
 const Dialog = () => {
   return (
-    <h1>
-      Это диалог !!!
-    </h1>
+    <div className='Dialog'>
+      <div className='Dialog--item HeaderBlock'>
+        <div className='HeaderBlock--item DialogName'>Elizabeth Turner</div>
+        <div className='HeaderBlock--item'>
+          <div className='HeaderBlock--search'>
+            <Input placeholder='Найти сообщение' />
+          </div>
+        </div>
+      </div>
+      <div className='Dialog--item DialogContent'>
+        <DialogMessage />
+        <DialogMessage />
+        <DialogMessage />
+        <DialogMessage />
+        <DialogMessage />
+        <DialogMessage />
+        <DialogMessage />
+        <DialogMessage />
+        <DialogMessage />
+      </div>
+      <div className='Dialog--item FooterBlock'>
+        <div className='AnswerBlock'>
+          <LabelInput label='Введите ответ' />
+          <select>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+          </select>
+        </div>
+      </div>
+    </div>
   )
 }
 
